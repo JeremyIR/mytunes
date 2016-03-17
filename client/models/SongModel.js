f63b0950c39661a4936f09237c5fd84bab639595
JeremyIR <jeremy.i.rosenfeld@gmail.com>
2016-03-17 10:29:14 -0700

    Add a dequeue event to SongModel

--- client/models/SongModel.js
+++ client/models/SongModel.js
@@ -9,5 +9,10 @@ var SongModel = Backbone.Model.extend({
   enqueue: function() {
     // Triggering an event here will also trigger the event on the collection
     this.trigger('enqueue', this);
+  },
+
+  dequeue: function() {
+     // Triggering an event here will also trigger the event on the collection
+    this.trigger('dequeue', this);   
   }
 });
