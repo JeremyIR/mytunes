f55ae21779a1cd9ad8f2fc66a1436700be2f125b
JeremyIR <jeremy.i.rosenfeld@gmail.com>
2016-03-17 19:35:32 -0700

    Render the Song Queue with the app

--- client/views/AppView.js
+++ client/views/AppView.js
@@ -4,18 +4,30 @@ var AppView = Backbone.View.extend({
   initialize: function(params) {
     this.playerView = new PlayerView({model: this.model.get('currentSong')});
     this.libraryView = new LibraryView({collection: this.model.get('library')});
+    this.songQueueView = new SongQueueView({collection: this.model.get('songQueue')});
 
     // change:currentSong - this is Backbone's way of allowing you to filter events to
     // ONLY receive change events for the specific property, 'currentSong'
     this.model.on('change:currentSong', function(model) {
       this.playerView.setSong(model.get('currentSong'));
     }, this);
+
+    this.listenTo(this.model.get('songQueue'), 'playFirst', function(songQueue) {
+      this.playerView.model.set(songQueue.at(0));
+    }, this);
+
+    // this.listenTo(this.model.get('songQueue'), 'change', function() {
+    //   debugger;
+    //   this.playerView.model.set(this.model.get('songQueue').at(0));
+    // }, this);
+
   },
 
   render: function() {
     return this.$el.html([
       this.playerView.$el,
-      this.libraryView.$el
+      this.libraryView.$el,
+      this.songQueueView.$el
     ]);
   }
 
