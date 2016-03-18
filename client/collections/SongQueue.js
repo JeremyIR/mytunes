99c015094dd809cdd8628e2bfcbfbfd78701177f
JeremyIR <jeremy.i.rosenfeld@gmail.com>
2016-03-17 19:37:56 -0700

    Properly dequeues a song when clicked

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -1,12 +1,14 @@
-// SongQueue.js - Defines a backbone model class for the song queue.
+// SongQueue.js - Defines a backbone model (collection?) class for the song queue.
 var SongQueue = Songs.extend({
 
   initialize: function() {
+
     this.on('add', function() {
       if (!this.at(1)) {
         this.playFirst(this.at(0));
       }
     }, this);
+
     this.on('ended', function() {
       this.remove(this.at(0));
       if (this.at(0)) {
@@ -14,15 +16,15 @@ var SongQueue = Songs.extend({
       }
     }, this);
 
-    this.on('dequeue', function(arg) {
-      this.remove(arg);
+    this.on('dequeue', function(song) {
+      this.remove(song);
     }, this);
-
 
   },
 
   playFirst: function() {
     this.at(0).play();
+    this.trigger('playFirst', this);
   },
 
 });
